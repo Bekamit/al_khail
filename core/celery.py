09bdf8f36a8e3ec6fa06a9fc6144a135e7353bf2
BekhanRay <bekhanrayamjanov@gmail.com>
2023-12-28 22:11:45 +0600

    Bekhan init

--- core/celery.py
+++ core/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 from celery.beat import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
