a381e98f26be086fb67cfb52d2587b31a326bdd8
BekhanRay <bekhanrayamjanov@gmail.com>
2024-01-22 19:08:22 +0600

    UPD 22 Delay in appeal was deleted

--- core/settings/base.py
+++ core/settings/base.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 PRODUCTION = env('PRODUCTION', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    "http://localhost:5173",
-    "http://localhost:6379",
-    "http://localhost:8000",
-    "http://localhost:654",
-    "http://16.171.129.40",
-    "https://gulsdem.pp.ua",
-    "http://gulsdem.pp.ua",
-    # "*",
+    # "http://localhost:5173",
+    # "http://localhost:6379",
+    # "http://localhost:8000",
+    # "http://localhost:654",
+    # "http://16.171.129.40",
+    # "https://gulsdem.pp.ua",
+    # "http://gulsdem.pp.ua",
+    "*",
     # 'gulsdem.pp.ua',
     # 'localhost',
 ]
