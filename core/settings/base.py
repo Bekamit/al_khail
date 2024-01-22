ccf22d314e822987e7e19640945801a0303fd3fc
BekhanRay <bekhanrayamjanov@gmail.com>
2024-01-22 20:09:35 +0600

    UPD 22 Delay in appeal was deleted

--- core/settings/base.py
+++ core/settings/base.py
@@ -55,7 +55,7 @@ THEME = [
 ]
 APPS = [
     'apps.admin_app',
-    'apps.appeal',
+    # 'apps.appeal',
     'apps.city',
     'apps.company',
     'apps.estate',
@@ -197,14 +197,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'back_media')
 
 
 # EMAIL
-EMAIL_BACKEND = config.EMAIL_BACKEND
-EMAIL_HOST = config.EMAIL_HOST
-EMAIL_PORT = config.EMAIL_PORT
-EMAIL_USE_TLS = config.EMAIL_USE_TLS
-EMAIL_HOST_USER = config.EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = config.EMAIL_HOST_PASSWORD
-DEFAULT_FROM_EMAIL = config.DEFAULT_FROM_EMAIL
-SERVER_EMAIL = config.SERVER_EMAIL
+# EMAIL_BACKEND = config.EMAIL_BACKEND
+# EMAIL_HOST = config.EMAIL_HOST
+# EMAIL_PORT = config.EMAIL_PORT
+# EMAIL_USE_TLS = config.EMAIL_USE_TLS
+# EMAIL_HOST_USER = config.EMAIL_HOST_USER
+# EMAIL_HOST_PASSWORD = config.EMAIL_HOST_PASSWORD
+# DEFAULT_FROM_EMAIL = config.DEFAULT_FROM_EMAIL
+# SERVER_EMAIL = config.SERVER_EMAIL
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
